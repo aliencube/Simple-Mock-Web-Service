5647f29603f9a4679b3d9abd78a8ec49d9adaee2
Justin Yoo <justin.yoo@aliencube.com>
2013-10-28 14:56:53 +1100

    Updated Web API controller.

---
+++ SourceCodes/SimpleMockWebService.Web.API.Tests/ServiceControllerTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+
+namespace SimpleMockWebService.Web.API.Tests
+{
+    [TestFixture]
+    public class ServiceControllerTest
+    {
+        #region SetUp / TearDown
+
+        [SetUp]
+        public void Init()
+        { }
+
+        [TearDown]
+        public void Dispose()
+        { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        #endregion
+    }
+}
