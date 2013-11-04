6896fd609fa47364675facabe85eee877d46a65c
Justin Yoo <justin.yoo@aliencube.com>
2013-11-04 11:13:17 +1100

    Added javascript, css library files.

---
+++ javascripts/controllers.js
@@ -0,0 +1,17 @@
+﻿(function (angular) {
+    var module = angular.module("markdownApp", []);
+
+    module.value("converter", function ($scope, $http) {
+        return converterService($scope, $http);
+    });
+
+    module.controller("markdownController", function ($scope, $http, converter) {
+        $scope.init = function () {
+            $scope.converterService = converter($scope, $http);
+        };
+
+        $scope.getHtml = function (filename) {
+            return $scope.converterService.getHtml(filename);
+        };
+    });
+})(angular);
---
+++ javascripts/services.js
@@ -0,0 +1,9 @@
+﻿var converterService = function ($scope, $http) {
+    var getHtml = function (filename) {
+        $http.get(filename).success(function (data) {
+            var converter = new Markdown.Converter().makeHtml;
+            var html = converter(data);
+            return html;
+        });
+    };
+};
