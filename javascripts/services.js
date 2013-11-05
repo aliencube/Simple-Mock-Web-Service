c9762366b7e8ab44eebbb159ffb464ff3f3d1c2a
Justin Yoo <justin.yoo@aliencube.com>
2013-11-05 22:51:23 +1100

    Updated index.html.

--- javascripts/services.js
+++
@@ -1,9 +0,0 @@
-﻿var converterService = function ($scope, $http) {
-    var getHtml = function (filename) {
-        $http.get(filename).success(function (data) {
-            var converter = new Markdown.Converter().makeHtml;
-            var html = converter(data);
-            return html;
-        });
-    };
-};
