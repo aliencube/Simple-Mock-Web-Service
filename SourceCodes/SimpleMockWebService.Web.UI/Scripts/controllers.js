f99873a9b83de4f40556e6f4fc4ceb92310358fb
Justin Yoo <justin.yoo@aliencube.com>
2013-11-25 10:16:59 +1100

    Added controller logic.

--- SourceCodes/SimpleMockWebService.Web.UI/Scripts/controllers.js
+++ SourceCodes/SimpleMockWebService.Web.UI/Scripts/controllers.js
@@ -1,1 +1,19 @@
-
+(function (angular) {
+    var module = angular.module("demoApp", []);
+    module.controller("demoController", function ($scope, $http) {
+        $scope.getHtml = function (method, url) {
+            var config = {
+                "method": method,
+                "url": url
+            };
+            if (method == "post" || method == "put") {
+                config.data = "=" + JSON.stringify({ "data": "dummy" });
+                config.headers = { "Content-Type": "application/x-www-form-urlencoded" };
+            }
+            $http(config)
+                .success(function (data) {
+                    $scope.json = JSON.stringify(data, null, "    ");
+                });
+        };
+    });
+})(angular);
