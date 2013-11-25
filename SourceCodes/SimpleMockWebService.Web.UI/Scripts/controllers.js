77b89a10443cec6b803832096d61340387117624
Justin Yoo <justin.yoo@aliencube.com>
2013-11-25 11:12:15 +1100

    Updated to use "ng-repeat".

--- SourceCodes/SimpleMockWebService.Web.UI/Scripts/controllers.js
+++ SourceCodes/SimpleMockWebService.Web.UI/Scripts/controllers.js
@@ -1,6 +1,35 @@
 (function (angular) {
     var module = angular.module("demoApp", []);
+    
     module.controller("demoController", function ($scope, $http) {
+        $scope.items = [
+            {
+                "title": "Get List of Contents",
+                "method": "get",
+                "url": "/api/contents"
+            },
+            {
+                "title": "Get Content #1",
+                "method": "get",
+                "url": "/api/content/1"
+            },
+            {
+                "title": "Post Content",
+                "method": "post",
+                "url": "/api/content"
+            },
+            {
+                "title": "Put Content #1",
+                "method": "put",
+                "url": "/api/content/1"
+            },
+            {
+                "title": "Delete Content #1",
+                "method": "delete",
+                "url": "/api/content/1"
+            }
+        ];
+
         $scope.getHtml = function (method, url) {
             var config = {
                 "method": method,
