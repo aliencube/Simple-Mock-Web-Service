25dfe4fd552f260740b2510f6db0b6db39109963
Justin Yoo <justin.yoo@aliencube.com>
2013-11-25 16:22:30 +1100

    Refactored controllers.js by separating methods from controller to service.

--- SourceCodes/SimpleMockWebService.Web.UI/Scripts/controllers.js
+++ SourceCodes/SimpleMockWebService.Web.UI/Scripts/controllers.js
@@ -1,53 +1,17 @@
 (function (angular) {
     var module = angular.module("demoApp", []);
     
-    module.controller("demoController", function ($scope, $http) {
-        $scope.items = [
-            {
-                "title": "Get List of Contents",
-                "method": "get",
-                "url": "/api/contents",
-                "result": "<- Click for result"
-            },
-            {
-                "title": "Get Content #1",
-                "method": "get",
-                "url": "/api/content/1",
-                "result": "<- Click for result"
-            },
-            {
-                "title": "Post Content",
-                "method": "post",
-                "url": "/api/content",
-                "result": "<- Click for result"
-            },
-            {
-                "title": "Put Content #1",
-                "method": "put",
-                "url": "/api/content/1",
-                "result": "<- Click for result"
-            },
-            {
-                "title": "Delete Content #1",
-                "method": "delete",
-                "url": "/api/content/1",
-                "result": "<- Click for result"
-            }
-        ];
+    module.value("demo", function ($scope, $http) {
+        return demoService($scope, $http);
+    });
+
+    module.controller("demoController", function ($scope, $http, demo) {
+        $scope.demoService = demo($scope, $http);
+
+        $scope.items = $scope.demoService.items;
 
         $scope.getJsonResult = function (index, method, url) {
-            var config = {
-                "method": method,
-                "url": url
-            };
-            if (method == "post" || method == "put") {
-                config.data = "=" + JSON.stringify({ "data": "dummy" });
-                config.headers = { "Content-Type": "application/x-www-form-urlencoded" };
-            }
-            $http(config)
-                .success(function (data) {
-                    $scope.items[index].result = JSON.stringify(data, null, "    ");
-                });
+            $scope.demoService.getJsonResult(method, url, $scope.items[index]);
         };
     });
 })(angular);
